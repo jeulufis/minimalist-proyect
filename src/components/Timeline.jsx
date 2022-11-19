9181aa9b765108c173f7e5ca8dad9ac9fe181a7a
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-11-18 22:28:05 -0300

    Modicando texto

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -18,7 +18,7 @@ function Timeline() {
                ))}
            <div className='pt-5'>
            <Title>I ♥</Title>
-           <p className='text-sm'>Music, Read, Walk,<span className='text-blue-500 dark:text-pink-500'>Video Games</span>, Lear New Languag </p>
+           <p className='text-sm'>Music, Read, Walk,<span className='text-blue-500 dark:text-pink-500'>Video Games</span>, Lear New Language.</p>
            </div>
          </div>
       </div>
--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 function TimelineItem({ year, title }) {
    return (
       <>
-
-
          <ol className="flex flex-col md:flex-row relative border-l border-gray-600 dark:border-stone-700">
             <li className="mb-6 ml-4" >
                <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-black dark:border-stone-900 dark:bg-[#c9a87d]" />
