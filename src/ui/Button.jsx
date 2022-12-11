c1550bb0aef5f8d39e95c4049268e87d4561e3df
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-12-11 15:40:25 -0300

    EDIT: Color-text

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const Button = ({ logo, name, direction }) => {
   return (
     <a href={direction} target='_blank'><button
-    className="px-3 py-2 flex place-items-center gap-2 text-black font-medium text-sm
-            rounded-md hover:text-emerald-600 hover:border-b-blue-400 hover:border-b-2 focus:bg-gray-300 
+    className="px-3 py-2 flex place-items-center gap-2 text-emerald-700 text-sm
+            rounded-md hover:text-emerald-500 hover:border-b-blue-400 hover:border-b-2 focus:bg-gray-300 
           focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg 
           transition duration-150 ease-in-out dark:text-orange-200"
   >
