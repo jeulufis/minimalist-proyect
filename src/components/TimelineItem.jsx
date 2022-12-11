58c71a2f07e6e91c106784a10313ba643ebd5d8b
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-12-11 17:40:26 -0300

    Change Code Timeline

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -4,13 +4,13 @@ function TimelineItem({ year, title }) {
   return (
     <>
       <ol className="flex flex-col md:flex-row relative border-l border-gray-600 dark:border-stone-700 ">
-        <li className="mb-1 ml-4">
+        <li className="mb-4 ml-4">
           <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-black dark:border-stone-800" />
-          <h3 className="dark:text-white text-sm">
-            <span className="px-3 py-1 text-white dark:text-stone-900 bg-stone-900 dark:bg-white rounded-md font-bold mr-1">
+          <h3 className="dark:text-white text-sm flex items-center">
+            <span className="px-2 py-1 text-white absolute dark:text-stone-900 bg-stone-900 dark:bg-white rounded-md">
               {year}
             </span>{" "}
-            <span className="dark:text-stone-300 leading-7">
+            <span className="dark:text-stone-300  pl-14 ml-1">
             {title}
             </span>
             
