274726957b7dcfd05426715975a075062f9627c0
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-12-15 18:28:03 -0300

    Update links

--- src/data/blog.js
+++ src/data/blog.js
@@ -8,12 +8,12 @@ export default [
      {
       title: 'Steals like an artist',
       imgUrl: 'https://media.istockphoto.com/id/1415239957/es/foto/ladrones-enmascarados-rompiendo-y-entrando-en-la-casa-de-una-v%C3%ADctima.jpg?s=612x612&w=0&k=20&c=m50nUrfeG0s798SxRuibVjhBFfzlrvnzi1Otn-7v41Y=',
-      link: 'https://github.com/itodev-source/clone-tesla',
+      link: 'https://itodev.blogspot.com/2022/12/steals-like-artist.html',
       parrafo: 'Summary of the book "Steal like an artist" by Austin Kleon, to have a more artistic life.'
    }, {
       title: 'The intelligent investor',
       imgUrl: 'https://media.istockphoto.com/id/1061369956/es/foto/pilas-de-monedas-y-gr%C3%A1fico-gr%C3%A1ficos-en-un-tablero-de-ajedrez.jpg?s=612x612&w=0&k=20&c=QUK0VFnMjRMS6IR1Akl3kCfwO8dIeO3O0qSakEy48rY=',
-      link: '/',
+      link: 'https://itodev.blogspot.com/2022/12/el-inversor-inteligente-resumen.html',
       parrafo: 'A smart investor is not one who looks for short-term returns, but those who look for the long term.'
    },{
       title: 'Learn how to promote your work',
