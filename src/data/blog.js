423cb8b9de6962b851c8531cec297813ee21ca2f
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-12-17 15:10:48 -0300

    Add new blog

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,24 +1,34 @@
 export default [
-   {
-      title: 'Atomic Habits',
-      imgUrl: 'https://cdn.pixabay.com/photo/2014/02/01/17/28/apple-256261_1280.jpg',
-      link: 'https://itodev.blogspot.com/2022/12/habitos-atomicos-resumen.html',
-      parrafo: 'A summary of the book Atomic Habits based on James Clear book, to achieve better routines.'
-   },
-     {
-      title: 'Steals like an artist',
-      imgUrl: 'https://media.istockphoto.com/id/1415239957/es/foto/ladrones-enmascarados-rompiendo-y-entrando-en-la-casa-de-una-v%C3%ADctima.jpg?s=612x612&w=0&k=20&c=m50nUrfeG0s798SxRuibVjhBFfzlrvnzi1Otn-7v41Y=',
-      link: 'https://itodev.blogspot.com/2022/12/steals-like-artist.html',
-      parrafo: 'Summary of the book "Steal like an artist" by Austin Kleon, to have a more artistic life.'
-   }, {
-      title: 'The intelligent investor',
-      imgUrl: 'https://media.istockphoto.com/id/1061369956/es/foto/pilas-de-monedas-y-gr%C3%A1fico-gr%C3%A1ficos-en-un-tablero-de-ajedrez.jpg?s=612x612&w=0&k=20&c=QUK0VFnMjRMS6IR1Akl3kCfwO8dIeO3O0qSakEy48rY=',
-      link: 'https://itodev.blogspot.com/2022/12/el-inversor-inteligente-resumen.html',
-      parrafo: 'A smart investor is not one who looks for short-term returns, but those who look for the long term.'
-   },{
-      title: 'Learn how to promote your work',
-      imgUrl: 'https://cdn.pixabay.com/photo/2015/01/08/18/26/man-593333__480.jpg',
-      link: '/',
-      parrafo: 'Made for people looking to improve their job performance and for people who need an audience.'
-   },
-]
+  {
+    title: "Learn how to promote your work",
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/26/man-593333__480.jpg",
+    link: "/",
+    parrafo:
+      "Made for people looking to improve their job performance and for people who need an audience.",
+  },
+  {
+    title: "Atomic Habits",
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2014/02/01/17/28/apple-256261_1280.jpg",
+    link: "https://itodev.blogspot.com/2022/12/habitos-atomicos-resumen.html",
+    parrafo:
+      "A summary of the book Atomic Habits based on James Clear book, to achieve better routines.",
+  },
+  {
+    title: "Steals like an artist",
+    imgUrl:
+      "https://media.istockphoto.com/id/1415239957/es/foto/ladrones-enmascarados-rompiendo-y-entrando-en-la-casa-de-una-v%C3%ADctima.jpg?s=612x612&w=0&k=20&c=m50nUrfeG0s798SxRuibVjhBFfzlrvnzi1Otn-7v41Y=",
+    link: "https://itodev.blogspot.com/2022/12/steals-like-artist.html",
+    parrafo:
+      'Summary of the book "Steal like an artist" by Austin Kleon, to have a more artistic life.',
+  },
+  {
+    title: "The intelligent investor",
+    imgUrl:
+      "https://media.istockphoto.com/id/1061369956/es/foto/pilas-de-monedas-y-gr%C3%A1fico-gr%C3%A1ficos-en-un-tablero-de-ajedrez.jpg?s=612x612&w=0&k=20&c=QUK0VFnMjRMS6IR1Akl3kCfwO8dIeO3O0qSakEy48rY=",
+    link: "https://itodev.blogspot.com/2022/12/el-inversor-inteligente-resumen.html",
+    parrafo:
+      "A smart investor is not one who looks for short-term returns, but those who look for the long term.",
+  },
+];
