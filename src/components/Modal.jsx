1580f5382f815e80dcd008b91a3173291e6837c9
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-05 22:26:45 -0300

    Less width modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,7 +25,7 @@ export const Modal = ({ closeModal, showModal, modalText, year }) => {
                 data-aos="zoom-in"
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
-                <div className="relative w-auto my-6 mx-auto  max-w-sm md:max-w-lg">
+                <div className="relative w-auto my-6 mx-auto max-w-xs md:max-w-lg">
                     {/*content*/}
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
