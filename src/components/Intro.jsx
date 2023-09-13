a3fbc4d77152ea7b61002611d4defef1cac6abc3
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-09-12 22:18:36 -0300

    cambio de letras responsive a el button My Blog

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -73,7 +73,7 @@ function Intro() {
               </p>
               <div className="self-center">
                 <Link to="/blog">
-                  <button className="py-2 text-[12px] transition ease-in-out delay-100 hover:-translate-y-0.5 hover:scale-110 duration-300 bg-emerald-400 hover:bg-emerald-300 dark:bg-emerald-500 dark:hover:bg-emerald-400 dark:hover:text-black md:px-7 px-6 rounded-md flex items-center text-black">
+                  <button className="py-2 text-sm md:text-base transition ease-in-out delay-100 hover:-translate-y-0.5 hover:scale-110 duration-300 bg-emerald-400 hover:bg-emerald-300 dark:bg-emerald-500 dark:hover:bg-emerald-400 dark:hover:text-black md:px-7 px-6 rounded-md flex items-center text-black">
                     My Blog
                     <AiOutlineDoubleRight className="ml-1" />
                   </button>
